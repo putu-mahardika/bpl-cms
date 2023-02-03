a3704a25365a06ffb50b5980eac347f3c4ecf4a4
Putu Mahardika <mahardikaputu13@gmail.com>
2023-02-03 15:16:53 +0700

    selesai revisi issue #53

--- config/controller/statusShipmentController.php
+++ config/controller/statusShipmentController.php
@@ -31,22 +31,25 @@
     while($data = mysqli_fetch_array($fetch_master_query)){
       if(strtolower($kode) == strtolower($data['kode'])){
         $j=1;
-        header("location:../../view/admin/inputStatusShipment.php");
-        $_SESSION['pesan'] = '<p><div class="alert alert-warning">Kode Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+        // header("location:../../view/admin/inputStatusShipment.php");
+        // $_SESSION['pesan'] = '<p><div class="alert alert-warning">Kode Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+        break;
       }
       elseif (strtolower($nama) == strtolower($data['nama'])) {
-        $j=1;
-        header("location:../../view/admin/inputStatusShipment.php");
-        $_SESSION['pesan'] = '<p><div class="alert alert-warning">Nama Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+        $j=2;
+        // header("location:../../view/admin/inputStatusShipment.php");
+        // $_SESSION['pesan'] = '<p><div class="alert alert-warning">Nama Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+        break;
       }
       elseif ($no == $data['atr1']) {
-        $j=1;
-        header("location:../../view/admin/inputStatusShipment.php");
-        $_SESSION['pesan'] = '<p><div class="alert alert-warning">Nomor Urutan Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+        $j=3;
+        // header("location:../../view/admin/inputStatusShipment.php");
+        // $_SESSION['pesan'] = '<p><div class="alert alert-warning">Nomor Urutan Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+        break;
       }
     }
 
-    if($j != 1){
+    if($j == 0){
       $query = "insert into master_status_shipment values(null, '$kode', '$nama', '$isActive', '$datetime', '$datetime', '$s_id', '$no', '', '')";
       $result = mysqli_query($koneksi, $query);
       if ($result) {
@@ -58,10 +61,23 @@
           header("location:../../view/admin/statusShipment.php");
           $_SESSION['pesan'] = '<p><div class="alert alert-warning">Data gagal ditambahkan !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';				
       }
-    } else {
-        // echo 'ccc';
-        header("location:../../view/admin/inputStatusShipment.php");
-        $_SESSION['pesan'] = '<p><div class="alert alert-warning">Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+    } 
+    elseif ($j == 1) {
+      header("location:../../view/admin/inputStatusShipment.php");
+      $_SESSION['pesan'] = '<p><div class="alert alert-warning">Kode Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+    }
+    elseif ($j == 2) {
+      header("location:../../view/admin/inputStatusShipment.php");
+      $_SESSION['pesan'] = '<p><div class="alert alert-warning">Nama Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+    }
+    elseif ($j == 3) {
+      header("location:../../view/admin/inputStatusShipment.php");
+      $_SESSION['pesan'] = '<p><div class="alert alert-warning">Nomor Urutan Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+    }
+    else {
+      // echo 'ccc';
+      header("location:../../view/admin/inputStatusShipment.php");
+      $_SESSION['pesan'] = '<p><div class="alert alert-warning">Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
     }
 
   } 
@@ -79,38 +95,43 @@
 			$isActive = 0;
 		}
 
-    while($data = mysqli_fetch_array($fetch_master_query)){
-      if((strtolower($kode) == strtolower($data['kode']) || strtolower($nama) == strtolower($data['nama']) || $no == $data['no'] ) && $id != $data['id']){
-          $j=1;
-          break;
-      } elseif((strtolower($kode) == strtolower($data['kode']) || strtolower($nama) == strtolower($data['nama']) || $no == $data['no'] ) && $id == $data['id']){
-          $j=0;
-          break;
-      }
-    }
+    $j=0;
+
+    // while($data = mysqli_fetch_array($fetch_master_query)){
+    //   if((strtolower($kode) == strtolower($data['kode']) || strtolower($nama) == strtolower($data['nama']) || $no == $data['no'] ) && $id != $data['id']){
+    //       $j=1;
+    //       break;
+    //   } elseif((strtolower($kode) == strtolower($data['kode']) || strtolower($nama) == strtolower($data['nama']) || $no == $data['no'] ) && $id == $data['id']){
+    //       $j=0;
+    //       break;
+    //   }
+    // }
 
     while($data = mysqli_fetch_array($fetch_master_query)){
       if(strtolower($kode) == strtolower($data['kode']) && $id != $data['id']){
         $j=1;
-        header("location:../../view/admin/inputStatusShipment.php?id=$id");
-        $_SESSION['pesan'] = '<p><div class="alert alert-warning">KodeStatus Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+        // header("location:../../view/admin/inputStatusShipment.php?id=$id");
+        // $_SESSION['pesan'] = '<p><div class="alert alert-warning">KodeStatus Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+        break;
       }
       elseif (strtolower($nama) == strtolower($data['nama']) && $id != $data['id']) {
-        $j=1;
-        header("location:../../view/admin/inputStatusShipment.php?id=$id");
-        $_SESSION['pesan'] = '<p><div class="alert alert-warning">Nama Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+        $j=2;
+        break;
+        // header("location:../../view/admin/inputStatusShipment.php?id=$id");
+        // $_SESSION['pesan'] = '<p><div class="alert alert-warning">Nama Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
       }
       elseif ($no == $data['atr1'] && $id != $data['id']) {
-        $j=1;
-        header("location:../../view/admin/inputStatusShipment.php?id=$id");
-        $_SESSION['pesan'] = '<p><div class="alert alert-warning">Nomor Urutan Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+        $j=3;
+        break;
+        // header("location:../../view/admin/inputStatusShipment.php?id=$id");
+        // $_SESSION['pesan'] = '<p><div class="alert alert-warning">Nomor Urutan Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
       }
     }
 
     // echo $_POST['aktif'];
     // echo $isActive;
 
-    if($j != 1) {
+    if($j == 0) {
       $query = "update master_status_shipment set kode='$kode', nama='$nama', aktif='$isActive', last_update='$datetime', UserId='$s_id', atr1='$no' where id='$id'";
       $result = mysqli_query($koneksi, $query);
       if ($result) {
@@ -120,7 +141,21 @@
           header("location:../../view/admin/statusShipment.php");
           $_SESSION['pesan'] = '<p><div class="alert alert-warning">Data gagal diubah !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';				
       }
-    } else {
+      
+    } 
+    elseif ($j==1) {
+      header("location:../../view/admin/inputStatusShipment.php?id=$id");
+      $_SESSION['pesan'] = '<p><div class="alert alert-warning">KodeStatus Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+    }
+    elseif ($j==2) {
+      header("location:../../view/admin/inputStatusShipment.php?id=$id");
+      $_SESSION['pesan'] = '<p><div class="alert alert-warning">Nama Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+    }
+    elseif ($j==3) {
+      header("location:../../view/admin/inputStatusShipment.php?id=$id");
+      $_SESSION['pesan'] = '<p><div class="alert alert-warning">Nomor Urutan Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
+    }
+    else {
         header("location:../../view/admin/editStatusShipment.php?id=$id");
         $_SESSION['pesan'] = '<p><div class="alert alert-warning">Status Shipment sudah ada !<a class="close" data-dismiss="alert" href="#">x</a></div></p>';
     }
